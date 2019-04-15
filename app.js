b8f3fe79d500e6f1e6030c0ed4aa405965e03354
Dipok Chakraborty <dipokchakraborty@gmail.com>
2019-04-15 14:48:28 +0600

    Update

--- app.js
+++ app.js
@@ -235,11 +235,11 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || 8080;
+var server_ip_address = server.address();
  
 server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+  console.log( "Listening on " + server_ip_address + ", port " + server_port );
 });
 
 
